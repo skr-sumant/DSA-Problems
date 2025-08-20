30a93e6822c646f06184ae745afe5ca8c3083e73
Sumant Kumar <153903818+skr-sumant@users.noreply.github.com>
2025-08-20 14:29:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.98%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -4,16 +4,6 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x < 2:
-            return x
 
-        left, right = 1, x // 2
-        while left <= right:
-            mid = (left + right) // 2
-            if mid * mid == x:
-                return mid
-            elif mid * mid < x:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return right
+        return int(sqrt(x))
+        
